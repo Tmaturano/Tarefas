c3b732ec85c2948adea2ab53716f1d36e2de47a0
Thiago Maturana de Oliveira <thiagomatu@hotmail.com>
2017-07-07 19:06:52 -0300

    Adicionando o migration e melhorando o mapeamento das entidades com o banco (de forma manual por causa do EF Core)

--- Tarefas.Infrastructure.Data/Context/TarefasEFContext.cs
+++ Tarefas.Infrastructure.Data/Context/TarefasEFContext.cs
@@ -2,41 +2,18 @@
 using System.IO;
 using Tarefas.Domain.Entities;
 using Microsoft.Extensions.Configuration;
+using Tarefas.Infrastructure.Data.EntityConfig;
+using Eventos.IO.Infra.Data.Extensions;
 
 namespace Tarefas.Infrastructure.Data.Context
 {
     public class TarefasEFContext : DbContext
     {
-        public TarefasEFContext()
-        {
-        }
-
         public DbSet<Tarefa> Tarefas { get; set; }
-
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Tarefa>()
-                .Property(t => t.Id)
-                .IsRequired();
-
-            modelBuilder.Entity<Tarefa>()
-                .Property(t => t.Status)
-                .IsRequired();
-
-            modelBuilder.Entity<Tarefa>()
-                .Property(t => t.Titulo)
-                .HasColumnType("varchar(50)")
-                .IsRequired();
-
-            modelBuilder.Entity<Tarefa>()
-                .Property(t => t.Descricao)
-                .HasColumnType("varchar(250)")
-                .IsRequired();
-
-            modelBuilder.Entity<Tarefa>()
-                .ToTable("Tarefas");
-
+            modelBuilder.AddConfiguration(new TarefasConfiguration());
             base.OnModelCreating(modelBuilder);
         }
 
